feee819d22ade65e6907be8f9552b967efa03b8f
PeterDaniel3D <75070076+PeterDaniel3D@users.noreply.github.com>
2021-11-25 08:34:28 +0100

    added DeckFacade

--- src/main/java/entities/Deck.java
+++ src/main/java/entities/Deck.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.Date;
 import javax.persistence.*;
 
 @Entity
@@ -14,15 +15,21 @@ public class Deck implements Serializable {
     private String deck_id;
 
     @Column(name = "created")
-    private Timestamp created;
+    private Long created;
 
     @Column(name = "completed")
-    private Timestamp completed;
+    private Long completed;
 
     @Column(name = "finished")
     private boolean finished;
 
     public Deck() {
+    }
+
+    public Deck(String deck_id) {
+        this.deck_id = deck_id;
+        Date date = new Date();
+        this.created = new Timestamp(date.getTime()).getTime();
     }
 
     public String getDeck_id() {
@@ -33,19 +40,19 @@ public class Deck implements Serializable {
         this.deck_id = deck_id;
     }
 
-    public Timestamp getCreated() {
+    public Long getCreated() {
         return created;
     }
 
-    public void setCreated(Timestamp created) {
+    public void setCreated(Long created) {
         this.created = created;
     }
 
-    public Timestamp getCompleted() {
+    public Long getCompleted() {
         return completed;
     }
 
-    public void setCompleted(Timestamp completed) {
+    public void setCompleted(Long completed) {
         this.completed = completed;
     }
 
